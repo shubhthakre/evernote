842a2c620f2a17f357b300ce566693f8dcb39d29
shubh <thakres955@gmail.com>
2021-04-24 01:01:04 +0530

    created the super evernote clone

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
+
+
+
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyAYlbyx89bO8GVsVxCoGJbRgQvA2AKgNZs",
+  authDomain: "evernote-557fc.firebaseapp.com",
+  projectId: "evernote-557fc",
+  storageBucket: "evernote-557fc.appspot.com",
+  messagingSenderId: "208097379069",
+  appId: "1:208097379069:web:970ca6bcf9bc224b68b494"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
